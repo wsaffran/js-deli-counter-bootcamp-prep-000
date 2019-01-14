7959ed7c3cee48efe34a1060d2042d8d23621fef
William Saffran <willh.saffran@gmail.com>
2019-01-14 13:09:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0)
+    return `There is nobody waiting to be served!`
+  else
+    return `Currently serving ${katzDeliLine.shift()}.`
+}
+
+function currentLine(line) {
+  var currentLine = []
+  var i = 0
+  while (i < line.length) {
+    currentLine.push(` ${i+1}. ${line[i]}`)
+    i++
+  }
+  if (currentLine.length === 0)
+    return `The line is currently empty.`
+  else {
+    return `The line is currently:${currentLine}`
+  }
+}
+
+
+
+
+
+
+
+
+
+/*
+function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(`${name}`)
   return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
@@ -23,3 +58,4 @@ function currentLine(katzDeliLine) {
   else
     return `The line is currently:${newLine}`
 }
+*/
